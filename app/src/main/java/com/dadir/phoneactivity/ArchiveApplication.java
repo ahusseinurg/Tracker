e61b332fae657a758df58015c4b33043c53019d1
ahusseinurg <ahussein@refugeegoats.org>
2026-08-27 22:42:20 -0600

    Add Phone Activity Android prototype: app/src/main/java/com/dadir/phoneactivity/ArchiveApplication.java

---
+++ app/src/main/java/com/dadir/phoneactivity/ArchiveApplication.java
@@ -0,0 +1,34 @@
+package com.dadir.phoneactivity;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+public class ArchiveApplication extends Application implements Application.ActivityLifecycleCallbacks {
+    private int started;
+    private boolean configurationChange;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(this);
+    }
+
+    @Override public void onActivityStarted(Activity activity) {
+        if (started == 0 && !configurationChange && !(activity instanceof PinActivity)) {
+            LockStore.lock();
+        }
+        started++;
+    }
+
+    @Override public void onActivityStopped(Activity activity) {
+        configurationChange = activity.isChangingConfigurations();
+        started--;
+        if (started == 0 && !configurationChange) LockStore.lock();
+    }
+
+    @Override public void onActivityCreated(Activity a, Bundle b) { }
+    @Override public void onActivityResumed(Activity a) { }
+    @Override public void onActivityPaused(Activity a) { }
+    @Override public void onActivitySaveInstanceState(Activity a, Bundle b) { }
+    @Override public void onActivityDestroyed(Activity a) { }
+}
